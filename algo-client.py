cbd5f08bb914526ac184e144217b4751bd311e1f
Carsten Kjær <ckjaer@bkmedical.com>
2021-03-22 15:44:44 +0100

    Change algo to larger buffers and send 10 times. Allow ping to VM

--- algo-client.py
+++ algo-client.py
@@ -1,5 +1,6 @@
 import socket
 import sys
+import time
 
 # Create a TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -9,22 +10,40 @@ server_address = (sys.argv[1], 10000)
 print(f'connecting to {server_address}')
 sock.connect(server_address)
 
-try:
-    
-    # Send data
-    message = b'This is the message.  It will be repeated.'
-    print(f'sending "{message}"')
-    sock.sendall(message)
+iterations = 10
 
-    # Look for the response
-    amount_received = 0
-    amount_expected = len(message)
-    
-    while amount_received < amount_expected:
-        data = sock.recv(16)
-        amount_received += len(data)
-        print(f'received "{data}"')
+totalStartTime = time.time()
 
-finally:
-    print('closing socket')
-    sock.close()
+for i in range(iterations):
+    try:
+        # Send data
+        message = b'This is the message.  It will be repeated.AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA'
+        print(f'sending data of size {len(message)}')
+        startTime = time.time()
+        sock.sendall(message)
+
+        # Look for the response
+        amount_received = 0
+        amount_expected = len(message)
+        
+        while amount_received < amount_expected:
+            data = sock.recv(16*1024)
+            amount_received += len(data)
+            print(f'received {len(data)} bytes')
+
+        print(f'send+recv took  {(time.time() - startTime)*1000.0*1000.0:.2f}us')
+
+    except socket.error as e:
+        print(f'exception {e}')
+        print('closing and reopening socket')
+        sock.close()
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect(server_address)
+
+
+sock.close()
+
+duration = (time.time() - totalStartTime)
+print(f'{iterations} iterations took {duration*1000.0*1000.0:.2f}us for an avg {(duration/iterations)*1000.0*1000.0:.2f}us')
+
+
--- algo-server.py
+++ algo-server.py
@@ -1,5 +1,6 @@
 import socket
 import sys
+import time
 
 # Create a TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -7,7 +8,7 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Bind the socket to the port
 server_address = ('', 10000)
-print(f'starting up on {server_address}')
+print(f'binding to {server_address}')
 sock.bind(server_address)
 
 # Listen for incoming connections
@@ -19,19 +20,20 @@ while True:
     connection, client_address = sock.accept()
 
     try:
+        connectTime = time.time()
         print(f'connection from {client_address}')
 
         # Receive the data in small chunks and retransmit it
         while True:
-            data = connection.recv(16)
-            print(f'received "{data}"')
+            data = connection.recv(16*1024)
+            print(f'\treceived data of size {len(data)}')
             if data:
-                print(f'sending data back to the client')
                 connection.sendall(data)
             else:
-                print(f'no more data from {client_address}')
+                print(f'\tno more data from {client_address}')
                 break
             
     finally:
         # Clean up the connection
         connection.close()
+        print(f'closed after {(time.time() - connectTime)*1000.0*1000.0:.2f}us')
