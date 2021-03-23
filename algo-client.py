8de7fe525c2dff22320a87a76e214749db1018c5
Carsten Kjær <ckjaer@bkmedical.com>
2021-03-23 14:38:30 +0100

    Improved client + server

--- algo-client.py
+++ algo-client.py
@@ -1,6 +1,16 @@
 import socket
 import sys
 import time
+import struct
+
+def recv_bytes(s, bytecount):
+    buf = bytearray(bytecount)
+    view = memoryview(buf)
+    while bytecount:
+        nbytes = s.recv_into(view, bytecount)
+        view = view[nbytes:] # slicing views is cheap
+        bytecount -= nbytes
+    return buf
 
 # Create a TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -17,30 +27,37 @@ totalStartTime = time.time()
 for i in range(iterations):
     try:
         # Send data
-        message = b'This is the message.  It will be repeated.AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA'
-        print(f'sending data of size {len(message)}')
+        payloadSize = 512**2
+        request = struct.pack('4s2i', bytes('abcd', 'ascii'), 0, payloadSize)
+        request += bytearray(payloadSize)
         startTime = time.time()
-        sock.sendall(message)
+        sock.sendall(request)
 
-        # Look for the response
-        amount_received = 0
-        amount_expected = len(message)
-        
-        while amount_received < amount_expected:
-            data = sock.recv(16*1024)
-            amount_received += len(data)
-            print(f'received {len(data)} bytes')
+        # Receive response
+        replyHeaderStruct = struct.Struct('4ci64si')
+
+        data = recv_bytes(sock, replyHeaderStruct.size)
+        header = replyHeaderStruct.unpack(data)
+
+        if (header[0] != b'e' or header[1] != b'f' or header[2] != b'g' or header[3] != b'h'):
+            raise socket.error(f'Invalid header {header[0:3]}')
+
+        status = header[4]
+        hostname = (header[5]).decode('ascii')
+        payloadSize = header[6]
+
+        payload = recv_bytes(sock, payloadSize)
 
         print(f'send+recv took  {(time.time() - startTime)*1000.0*1000.0:.2f}us')
-
     except socket.error as e:
         print(f'exception {e}')
         print('closing and reopening socket')
         sock.close()
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.connect(server_address)
-
-
+    except:
+        print(f'unknown exception')
+        raise
 sock.close()
 
 duration = (time.time() - totalStartTime)
--- algo-server.py
+++ algo-server.py
@@ -1,14 +1,25 @@
 import socket
 import sys
 import time
+import struct
+
+def recv_bytes(s, bytecount):
+    buf = bytearray(bytecount)
+    view = memoryview(buf)
+    while bytecount:
+        nbytes = s.recv_into(view, bytecount)
+        view = view[nbytes:] # slicing views is cheap
+        bytecount -= nbytes
+    return buf
+
+hostName = socket.gethostname()
 
 # Create a TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-
 # Bind the socket to the port
 server_address = ('', 10000)
-print(f'binding to {server_address}')
+print(f'Running on {hostName} - binding to {server_address}')
 sock.bind(server_address)
 
 # Listen for incoming connections
@@ -23,17 +34,34 @@ while True:
         connectTime = time.time()
         print(f'connection from {client_address}')
 
-        # Receive the data in small chunks and retransmit it
+        requestHeaderStruct = struct.Struct('4c2i')
+
         while True:
-            data = connection.recv(16*1024)
-            print(f'\treceived data of size {len(data)}')
-            if data:
-                connection.sendall(data)
-            else:
-                print(f'\tno more data from {client_address}')
-                break
-            
+            # Receive request
+            data = connection.recv(requestHeaderStruct.size)
+            header = requestHeaderStruct.unpack(data)
+            if (header[0] != b'a' or header[1] != b'b' or header[2] != b'c' or header[3] != b'd'):
+                raise Exception(f'Invalid header {header[0:4]}')
+            command = header[4]
+            payloadSize = header[5]
+            payload = recv_bytes(connection, payloadSize)
+
+            # Send response
+            response = struct.pack('4si64si', bytes('efgh', 'ascii'), 0, bytes(hostName, 'ascii'), payloadSize)
+            response += payload
+            connection.sendall(response)
+
+            print(f'\tReceived request with command {command} and payload of size {payloadSize} and returned response')
+    except socket.error as err:
+        print(f'exception {err}')
+    except Exception as err:
+        print(f'exception {err}')
+    except:
+        print(f'unknown exception')
     finally:
         # Clean up the connection
         connection.close()
         print(f'closed after {(time.time() - connectTime)*1000.0*1000.0:.2f}us')
+
+
+
